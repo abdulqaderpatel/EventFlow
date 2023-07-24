435f3e21f75fa55356f446d518aa4364b805b83a
abdulqaderpatel <abdulqaderpatel2002@gmail.com>
2023-07-24 22:11:11 +0530

    basic changes

---
+++ lib/Screens/User/search_friends.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchFriendsScreen extends StatelessWidget {
+  const SearchFriendsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello");
+  }
+}
